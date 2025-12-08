53193a5b0908d22d2e340c84c4c261d53dede9f6
chicokulit <samanthadevera12345@gmail.com>
2025-12-08 14:18:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  // Demo login → redirect sa portal.html
+  window.location.href = "portal.html";
+});
