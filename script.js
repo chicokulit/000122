c9052f2307ab2515dba6f3c7044b9cff9c85e98c
chicokulit <samanthadevera12345@gmail.com>
2025-12-08 14:34:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-// Login redirect
-const loginForm = document.getElementById("loginForm");
-if(loginForm){
-  loginForm.addEventListener("submit", function(e){
-    e.preventDefault();
-    window.location.href = "portal.html";
-  });
-}
+
